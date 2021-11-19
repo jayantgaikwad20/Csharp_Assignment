61c637a54309bcc478a582acb09205a5d1963905
Jayant  Gaikwad <92709730+jayantgaikwad20@users.noreply.github.com>
2021-11-19 20:13:22 +0530

    Add files via upload

---
+++ Day_1_Assignment/Program_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Fibonacci_Serise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 0, n2 = 1, n3;
+            Console.WriteLine(" " + n1);
+            int no = Convert.ToInt32(Console.ReadLine());
+
+            for(int i=0;i<=no;i++)
+            {
+                n3 = n1 + n2;
+                Console.WriteLine(" "+n3);
+                n1 = n2;
+                n2 = n3;
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
