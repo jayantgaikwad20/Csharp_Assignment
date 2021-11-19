4b0c5d7a32fede19c5f6958fdf854b59714faa1d
Jayant  Gaikwad <92709730+jayantgaikwad20@users.noreply.github.com>
2021-11-19 20:17:04 +0530

    Add files via upload

--- Day_1_Assignment/Program_2/Program.cs
+++ Day_1_Assignment/Program_2/Program.cs
@@ -1,1 +1,39 @@
-fgdfgdfggdffsssssssssssvdffffffffffffffffffffffff
+﻿using System;
+
+namespace Daya_In_Months
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter eny no from 1 to 12");
+            int MonthNo = Convert.ToInt32(Console.ReadLine());
+            switch (MonthNo)
+            {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 9:
+                case 11:
+                    Console.WriteLine("day in month is 31 ");
+                    break;
+                case 4:
+                case 6:
+                case 8:
+                case 10:
+                    Console.WriteLine("day in month is 30");
+                    break;
+                case 2:
+                    Console.WriteLine("day in month is 30");
+                    break;
+
+                    
+                    Console.ReadKey();
+
+
+            }
+
+        }
+    }
+}
