491c5544b98b09f28fb22803c904cf74b192a90d
Jayant  Gaikwad <92709730+jayantgaikwad20@users.noreply.github.com>
2021-11-25 14:57:45 +0530

    Add files via upload

---
+++ Day_2_Assignments/Program_6/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace SecondDay_Assignment2_Abstract_Animal
+{
+
+    abstract class Animal {
+
+        public abstract void foodHabit();
+
+    }
+
+
+    class Herbivorous : Animal {
+
+        public override void foodHabit()
+        {
+            Console.WriteLine("Consume only plants.");
+
+        }
+    }
+
+
+    
+
+        class Carnivorous : Animal
+    {
+
+        public override void foodHabit()
+        {
+            Console.WriteLine("Consume meat.");
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Herbivorous animals ");
+            new Herbivorous().foodHabit();
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Carnivorous animals ");
+            new Carnivorous().foodHabit();
+
+
+            Console.ReadKey();
+        }
+    }
+}
