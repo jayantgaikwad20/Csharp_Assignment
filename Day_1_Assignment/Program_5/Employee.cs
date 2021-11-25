7ebaeb75555f5ffe2c520df14548be07ed4e535e
Jayant  Gaikwad <92709730+jayantgaikwad20@users.noreply.github.com>
2021-11-25 14:22:16 +0530

    Add files via upload

---
+++ Day_1_Assignment/Program_5/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Employee
+{
+    class Employee
+    {
+        int empno;
+        String ename;
+        String des;
+
+        public Employee(int empno,String ename,String des)
+        {
+            this.empno = empno;
+            this.ename = ename;
+            this.des = des;
+        }
+        public void display()
+        {
+            Console.WriteLine("employee no is  " + empno);
+            Console.WriteLine("employee name is  " + ename);
+            Console.WriteLine("employee designation is" + des);        
+        }
+        static void Main(string[] args)
+        {
+            Employee e = new Employee(1,"jayant","developer");
+            e.display();
+
+
+        }
+    }
+}
