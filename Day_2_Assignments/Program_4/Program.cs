ae7ce45a6cd78e450c8fa4adea34785bc46a6a19
Jayant  Gaikwad <92709730+jayantgaikwad20@users.noreply.github.com>
2021-11-25 14:41:38 +0530

    Add files via upload

--- Day_2_Assignments/Program_4/Program.cs
+++ Day_2_Assignments/Program_4/Program.cs
@@ -1,1 +1,32 @@
-reth
+﻿using System;
+
+namespace SecondDay_Assignment2_ParaArray
+{
+
+
+   /* Write Average function to verify working param array.Pass different no of parameters and verify*/
+    class Program
+    {
+
+
+
+        public static int Average(params int[] no)
+        {
+            int sum = 0,avg=0;
+            for (int i = 0; i < no.Length; i++)
+            {
+                sum =sum+ no[i];
+            }
+
+            avg = sum / no.Length;
+            return avg;
+        }
+
+      
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Average(1,2,3));
+            Console.ReadKey();
+        }
+    }
+}
