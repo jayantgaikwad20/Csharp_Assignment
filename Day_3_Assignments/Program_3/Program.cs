e871d587f520da9b4e04420b5e69aa347c3555bd
Jayant  Gaikwad <92709730+jayantgaikwad20@users.noreply.github.com>
2021-11-25 16:38:06 +0530

    Add files via upload

---
+++ Day_3_Assignments/Program_3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+//Rushikesh Patil
+namespace ThirdDay_Assignment3_Program3_Interface
+{
+
+
+   /*   Rushikesh Patil 
+    *   
+    *   Program 3 -->
+    *   
+    *   Create interface with name IPrintable and print method.implement
+        interface in Account and employee class. 
+        Verify in main class and check working.*/
+
+    interface IPrintable
+    {
+        void print();
+    }
+
+    class Account : IPrintable
+    {
+        string acc_Type="Saving";
+         public void print()
+        {
+            Console.WriteLine("Account class Print method");
+         //   Console.WriteLine(acc_Type);
+        }
+    }
+
+
+    class Employee : IPrintable
+    {
+        string Emp_Type = "Permanent";
+        public void print()
+        {
+            Console.WriteLine("Employee class Print method");
+          //  Console.WriteLine(Emp_Type);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Account().print();
+
+            new Employee().print();
+
+            Console.ReadKey();
+        }
+    }
+}
