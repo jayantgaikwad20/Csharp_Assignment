a4c50f9221372a9515020cf94f45861f4f19842c
Jayant  Gaikwad <92709730+jayantgaikwad20@users.noreply.github.com>
2021-11-25 16:50:05 +0530

    Add files via upload

---
+++ Day_3_Assignments/Program_6/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ThirdDay_Assignment3_Program6_IndexOut
+{
+/*
+ * Rushikesh Patil
+ * 
+ * Program 6 -- >
+ *
+Create an array try to accept values from user handle IndexOutOfBounds Exception.
+*/
+    class Program
+    {
+        public static void ipArray(int [] a)
+        {
+
+            Console.Write("Input 5 elements in the array :\n");
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Array -- ");
+            foreach(int it in a)
+            {
+                Console.WriteLine(it);
+            }
+            // for exception accessing element more than size of array
+            a[6] = 1;
+
+        }
+
+        static void Main(string[] args)
+        {
+            int[] a = new int[5];
+
+
+            try
+            {
+                ipArray(a);
+        }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine("Array index out of bound--"+e.Message);
+            }
+
+            Console.ReadKey();
+}
+    }
+}
