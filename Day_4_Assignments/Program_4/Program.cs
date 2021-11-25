94385f0c2622dc868bcbc7307edd56803d533ed2
Jayant  Gaikwad <92709730+jayantgaikwad20@users.noreply.github.com>
2021-11-25 17:24:58 +0530

    Add files via upload

--- Day_4_Assignments/Program_4/Program.cs
+++ Day_4_Assignments/Program_4/Program.cs
@@ -1,1 +1,48 @@
-erkjuty
+﻿using System;
+using System.IO;
+
+namespace FourthDay_Assignment4_Program0_FIleHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+         /*   DriveInfo d = new DriveInfo("D:\\");
+            Console.WriteLine(d.Name+" "+d.DriveFormat);
+            Console.WriteLine(d.AvailableFreeSpace);
+            Console.WriteLine(d.IsReady);*/
+
+
+
+
+
+          /*  DirectoryInfo di = new DirectoryInfo("D:\\C#_Programs\\File_Handling");
+
+            if (di.Exists)
+            {
+                Console.WriteLine(di.FullName);
+                Console.WriteLine(di.CreationTime);
+                Console.WriteLine(di.Parent);
+
+                FileInfo[] f = di.GetFiles();
+
+                foreach(FileInfo fi in f)
+                {
+                    Console.WriteLine(fi.FullName);
+                    Console.WriteLine(fi.Extension);
+                    Console.WriteLine(fi.CreationTime);
+                    Console.WriteLine("--------------");
+                }
+            }
+            else
+            {
+                di.Create();
+                Console.WriteLine("Directory Created");
+            }*/
+
+
+            Console.ReadKey();
+        }
+    }
+}
