a93a8a20883859802d98801d2dfebfefc4516b61
Jayant  Gaikwad <92709730+jayantgaikwad20@users.noreply.github.com>
2021-12-02 13:41:56 +0530

    Add files via upload

---
+++ Assignments_Day_5/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+	
+	// Method to be called
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+
+ 	public int sub(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
