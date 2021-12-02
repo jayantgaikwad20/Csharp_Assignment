8c72030cfdb18720bfb992a088f2c61d0ab7e857
Jayant  Gaikwad <92709730+jayantgaikwad20@users.noreply.github.com>
2021-12-02 13:43:34 +0530

    Add files via upload

---
+++ Assignments_Day_5/Program_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace Program_StreamWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream("D:\\C#_Programs\\File_Handling\\sample.txt", FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(fs);
+            string fdata = reader.ReadToEnd();
+            Console.WriteLine("Data int file is :");
+            Console.WriteLine(fdata);
+            //StreamWriter writer = new StreamWriter(fs);
+            //writer.WriteLine("Hello c# File handling");
+            //writer.Flush();
+            fs.Close();
+            Console.Read();
+        }
+    }
+}
